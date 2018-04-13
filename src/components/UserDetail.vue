606120f65bbe93db71a0e9496db30513a773deee
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-13 10:10:55 +0200

    109 using custom events for child-parent

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,27 +3,35 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
-    export default{
-        props: {
-            myName: {
-                type: String,
-                default: 'Max'
-            }
-        },
-        methods: {
-            switchName(){
-                return this.myName.split("").reverse().join("");
-            }
-        }
+export default {
+  props: {
+    myName: {
+      type: String,
+      default: "Max"
     }
+  },
+  methods: {
+    switchName() {
+      return this.myName
+        .split("")
+        .reverse()
+        .join("");
+    },
+    resetName: function() {
+      this.myName = "Max";
+      this.$emit('nameWasReset', this.myName);
+    }
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+div {
+  background-color: lightcoral;
+}
 </style>
